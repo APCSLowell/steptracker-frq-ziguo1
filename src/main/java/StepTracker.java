489c3c5c00bd96562b4b6581a7477659acee04c0
ziguo1 <ziguo1@s.sfusd.edu>
2025-04-24 09:15:47 +0000

    wefjewoifjoiwjfweoifjewiojfewojfewof

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -2,4 +2,30 @@ import java.util.ArrayList;
 public class StepTracker
 {
  /* to be implemented here */
+ protected ArrayList<Integer> dps;
+ protected int tol; // post-grading addition
+
+ public StepTracker(int tol) {
+    dps = new ArrayList<Integer>();
+    this.tol = tol; // post-grading addition
+ }
+
+ public void addDailySteps(int steps) {
+    dps.add(steps);
+ }
+
+ public int activeDays() {
+    int cnt = 0;
+    for (int d : dps) {
+        if (d >= tol) cnt++; // post grading addition: d >= 10_000 ]=> d >= tol
+    }
+    return cnt;
+ }
+
+ public double averageSteps() {
+    if (dps.size() == 0) return 0D; // post-grading addition
+    double sum = 0;
+    for (int d : dps) sum += d;
+    return sum / dps.size();
+ }
 } 
